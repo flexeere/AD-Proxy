e10677fc9979ab5893c3e86267e994875ebcde8e
Arun Kushwaha <149176240+flexeere@users.noreply.github.com>
2023-10-28 18:49:46 +0530

    Update adv-add-user.sh

--- adv-add-user.sh
+++ adv-add-user.sh
@@ -24,20 +24,26 @@ GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 
-SQUID_USER=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
-SQUID_PW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+if [[ -d /etc/squid/ || -d /etc/squid3/ ]]; then
+    	echo "AD Proxy already installed."
+    	SQUID_USER=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
+	SQUID_PW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+	
+	htpasswd -b -c /etc/squid/passwd $SQUID_USER $SQUID_PW > /dev/null 2>&1
+	
+	sed -i 's/Squid proxy-caching web server/AD Proxy Service/g'  /etc/squid/squid.conf
+	
+	systemctl restart squid > /dev/null 2>&1
+	systemctl restart squid3 > /dev/null 2>&1
+	
+	echo -e "${NC}"
+	echo -e "${GREEN}Thank you for using AD Proxy Service.${NC}"
+	echo
+	echo -e "${CYAN}Username : ${SQUID_USER}${NC}"
+	echo -e "${CYAN}Password : ${SQUID_PW}${NC}"
+	echo -e "${CYAN}Port : 3128${NC}"
+	echo -e "${NC}"
+    exit 1
+fi
 
-htpasswd -b -c /etc/squid/passwd $SQUID_USER $SQUID_PW > /dev/null 2>&1
 
-sed -i 's/Squid proxy-caching web server/AD Proxy Service/g'  /etc/squid/squid.conf
-
-systemctl restart squid > /dev/null 2>&1
-systemctl restart squid3 > /dev/null 2>&1
-
-echo -e "${NC}"
-echo -e "${GREEN}Thank you for using AD Proxy Service.${NC}"
-echo
-echo -e "${CYAN}Username : ${SQUID_USER}${NC}"
-echo -e "${CYAN}Password : ${SQUID_PW}${NC}"
-echo -e "${CYAN}Port : 3128${NC}"
-echo -e "${NC}"
